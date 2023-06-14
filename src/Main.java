0988b13e5ecfbf211ecacc31918dd09f75f77326
batuhan.erkul <baerkul@hotmail.com>
2023-06-15 01:51:23 +0300

    Car Project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BMW bmw = new BMW("bmw", "320", 2020, 200, "jump");
+        bmw.print();
+
+        Audi audi = new Audi("audi", "Q7", 2020, 220, "jump");
+        audi.print();
+
+        Mercedes mercedes = new Mercedes();
+        mercedes.brandName = "mercedes";
+        System.out.println("Brand Name: "+mercedes.brandName);
+        mercedes.modelName = "G";
+        System.out.println("modelName: "+mercedes.modelName);
+        mercedes.modelYear = 2020;
+        System.out.println("modelYear: "+mercedes.modelYear);
+        mercedes.enginePower = 190;
+        System.out.println("enginePower: "+mercedes.enginePower);
+        mercedes.packetChoice = "jump";
+        System.out.println("packetChoice: "+mercedes.packetChoice);
     }
 }
--- src/Mercedes.java
+++ src/Mercedes.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Mercedes {
+public class Mercedes {
+    String brandName;
+    String modelName;
+    int modelYear;
+    int enginePower;
+    String packetChoice;
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getModelYear() {
+        return modelYear;
+    }
+
+    public void setModelYear(int modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public String getPacketChoice() {
+        return packetChoice;
+    }
+
+    public void setPacketChoice(String packetChoice) {
+        this.packetChoice = packetChoice;
+    }
 }
